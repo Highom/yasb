292a415d0ef82a19d56b73aecd53a24b071b0a49
Highom <highomlp@gmail.com>
2023-10-20 19:33:55 +0200

    changed media.py to handle no current session existing

--- scripts/media.py
+++ scripts/media.py
@@ -5,13 +5,16 @@ import json
 async def get_media_info():
     session_manager = await winsdk.windows.media.control.GlobalSystemMediaTransportControlsSessionManager.request_async()
     current_session = session_manager.get_current_session()
-    media_properties = await current_session.try_get_media_properties_async()
-    media_playback_info = {
-        "title": media_properties.title,
-        "artist": media_properties.artist,
-        "album": media_properties.album_title,
-        "album_artist": media_properties.album_artist,
-        "track": media_properties.track_number
-    }
-    print(json.dumps(media_playback_info))
+    if current_session:
+        media_properties = await current_session.try_get_media_properties_async()
+        media_playback_info = {
+            "titleAndArtist": media_properties.title + " - " + media_properties.artist
+        }
+        print(json.dumps(media_playback_info))
+    else:
+        media_playback_info = {
+            "titleAndArtist": ""
+        }
+        print(json.dumps(media_playback_info))
+
 asyncio.run(get_media_info())
